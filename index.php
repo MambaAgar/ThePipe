7d87e26646f18e63e291998682bea905a79ecf27
glyczak <gaba@gaba.space>
2017-06-08 15:15:48 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: glyczak
+ * Date: 6/8/17
+ * Time: 1:16 PM
+ */
+elements::header('Home');
+?>
+<div class="jumbotron">
+    <h1>Welcome to ThePipe</h1>
+    <p>
+        ThePipe is an experimental video streaming service written in PHP using IIS smooth streaming and H.264 video
+        encoding.  The service is only designed for development only and is thus, is not performance optimized.
+    </p>
+</div>
+<?php elements::footer(); ?>
